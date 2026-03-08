c57c56b55fc45dffd810d8456819c5ac59318433
Harris Crews <whcrews1998@gmail.com>
2026-03-08 13:59:31 -0400

    Centered board on page and updated style.

--- frontend/src/components/Board/index.jsx
+++ frontend/src/components/Board/index.jsx
@@ -2,15 +2,18 @@ import style from "./board.module.css";
 export default function Board() {
   return (
     <div className={style["container"]}>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
-      <div className={`${style["square"]}`}></div>
+      <p className={style["title"]}> Tic-Tac-Toe </p>
+      <div className={style["board"]}>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+        <div className={`${style["square"]}`}></div>
+      </div>
     </div>
   );
 }
