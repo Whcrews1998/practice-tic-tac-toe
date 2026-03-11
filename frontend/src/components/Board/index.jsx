6cef4272de09e9e1d772718b256409748084b655
Whcrews1998 <whcrews1998@gmail.com>
2026-03-11 09:08:21 -0500

    Created displayBoard() method.

--- frontend/src/components/Board/index.jsx
+++ frontend/src/components/Board/index.jsx
@@ -1,18 +1,35 @@
+import {useState} from "react";
 import style from "./board.module.css";
+
 export default function Board() {
+  const currentPlayer = "X";
+
+  const [board, setBoard] = useState(new Array(9).fill(null));
+
+  function displayBoard() {
+    return board.map( (value, index) => {
+      return (
+        <div key={index} className={style["square"]}>
+          {value}
+        </div>
+      );
+    })
+  }
+
   return (
     <div className={style["container"]}>
       <p className={style["title"]}> Tic-Tac-Toe </p>
       <div className={style["board"]}>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
-        <div className={`${style["square"]}`}></div>
+        {displayBoard()}
+        {/* <div className={`${style["square"]}`}>{board[0]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[1]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[2]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[3]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[4]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[5]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[6]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[7]}</div> */}
+        {/* <div className={`${style["square"]}`}>{board[8]}</div> */}
       </div>
     </div>
   );
