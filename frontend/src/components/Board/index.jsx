b8f2ae22ea451f3779219771a0ad9f18033c20f3
Whcrews1998 <whcrews1998@gmail.com>
2026-03-11 10:52:50 -0500

    Added checkForWinner method.

--- frontend/src/components/Board/index.jsx
+++ frontend/src/components/Board/index.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import style from "./board.module.css";
 
 let currentPlayer = "X";
@@ -17,7 +17,44 @@ export default function Board() {
     })
   }
 
-  function tickTile(index) {
+  function checkForWinnner() {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+
+    let hasWon = false;
+    for (let val of winningPatterns) {
+      const [a, b, c] = val;      
+      if (board[a] == null || board[b] == null || board[c] ==null)
+        continue;
+      if (board[a] === board[b] && board[b] === board[c])
+        hasWon = true;
+    }
+
+    // Check if winner found.
+    if (hasWon) {
+      alert("Winner!");
+      return;
+    }
+    // Check if all tiles are uses up.
+    if (!board.includes(null)) {
+      alert("Tie!");
+    }
+  }
+
+  useEffect(() => {
+    checkForWinnner();
+  }, [board]);
+
+  async function tickTile(index) {
     if (board[index] !== null) return;
 
     const newBoard = structuredClone(board);
@@ -25,7 +62,13 @@ export default function Board() {
 
     currentPlayer = (currentPlayer === "X") ? "O" : "X";
     setBoard(newBoard);
+
     return;
+  }
+
+  function resetGame() {
+    setBoard(new Array(9).fill(null));
+    currentPlayer = "X";
   }
 
   return (
@@ -34,6 +77,8 @@ export default function Board() {
       <div className={style["board"]}>
         {displayBoard()}
       </div>
+      <p> Player {currentPlayer}'s Turn</p>
+      <button type="button" onClick={resetGame}>Reset Game</button>
     </div>
   );
 }
